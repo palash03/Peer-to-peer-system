904af66d8b420d7a9c4081f343ebf3d5d22a0ebd
liblend <liblend@libltlnd-41274.lan>
2020-10-31 13:59:27 -0400

    working code for LOOKUP

--- client.py
+++ client.py
@@ -4,45 +4,34 @@ from _thread import *
 import pickle
 import random
 
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect((socket.gethostname(),7734))
-print(f"Connected to {socket.gethostname()} with port 7734")
-
-peerRfc = {} # The peers who have the RFC. Key: RFC number, Value: the list of peers having the RFC
-
-clienthost = '127.0.0.1'
-clientport = 20000 + random.randint(1,8000)
-
-# RFC directory
-currdir = os.listdir(os.getcwd()+'/RFC')
-count = 0
-for rfc in currdir:
-    if rfc.startswith('.'):
-        continue
-    peerRfc[count] = str(rfc[:-4])
-    count += 1
-print(peerRfc)
-
-def upload():
+def upload(clienthost,clientport):
+    print("Entering upload")
+    host = '0.0.0.0'
     sock = socket.socket()
-    sock.bind((clienthost,clientport))
+    sock.bind((host,clientport))
     sock.listen(5)
     while True:
         downloadSocket,downloadAddr = sock.accept()
         msgTransfer = downloadSocket.recv(1024)
         msg = pickle.loads(msgTransfer)
+        print("Print the msg now....")
         print(msg)
         downloadSocket.send()   
-        downloadSocket.close()
+        downloadSocket.close
 
-def addFileToClient(number,title):
-    res = "ADD RFC " + str(number) + " P2P-CI/1.0\n"\
+def performAdd(clienthost,clientport,number,title):
+    return "ADD RFC " + str(number) + " P2P-CI/1.0\n"\
            "HOST: " + str(clienthost) + "\n"\
            "Port: " + str(clientport) + "\n"\
            "Title: " + str(title)
-    return res
 
-def getUserInput():
+def performLookup(host,port,number,title):
+    return "LOOKUP " + str(number) + " P2P-CI/1.0" + "\n"\
+            "Host: " + str(host) + "\n"\
+            "Port: " + str(port) + "\n"\
+            "Title: " + str(title)
+
+def getUserInput(s,clienthost,clientport):
     print("Enter the function you want to perform: GET, LOOKUP, ADD, LIST, EXIT")
     inp = input()
     if inp == "ADD":
@@ -51,21 +40,66 @@ def getUserInput():
         print("Enter RFC Title: ")
         rfcTitle = input()
         directory = os.getcwd() + '/RFC/'
-        print("Directory: " + str(directory))
-        fileSelect = directory + str(rfcTitle) + ".txt"
-        print("File: " + str(fileSelect))
+        fileSelect = directory + str(rfcNum) + "-" + str(rfcTitle) + ".txt"
         if os.path.isfile(fileSelect):
-            data = addFileToClient(rfcNum,rfcTitle) 
-            #data = str(data[2:])
-            print(data)
-            info = pickle.dumps(data)
-            s.send(info)
+            data = performAdd(clienthost,clientport,rfcNum,rfcTitle) 
+            #print(data)
+            #info = pickle.dumps(bytes(data))
+            s.send(bytes(data,'utf-8'))
+            print("ADD response from server is: ")
             receive = s.recv(1024)
             print(receive)
         else:
             print("404 Error: File not found")
-        getUserInput()
+        getUserInput(s,clienthost,clientport)
+    
+    elif inp == "LOOKUP":
+        print("Enter RFC number: ")
+        rfcNum = input()
+        print("Enter RFC Title: ")
+        rfcTitle = input()          
+        getRfcDetails = performLookup(clienthost,clientport,rfcNum,rfcTitle)
+        print(getRfcDetails)
+        s.send(bytes(getRfcDetails,'utf-8'))
+        response = s.recv(1024)
+        print(response)
+        getUserInput(s,clienthost,clientport)
+    
+    elif inp == "EXIT":
+        s.send(bytes("EXIT",'utf-8'))
+        s.close
 
-# Threads for managing download requests from peers which remains active until connection is open
-start_new_thread(upload,())
-getUserInput()
+def main():
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.connect((socket.gethostname(),7734))
+    print(f"Connected to {socket.gethostname()} with port 7734")
+
+    peerRfc = {} # The peers who have the RFC. Key: RFC number, Value: the list of peers having the RFC
+
+    clienthost = '127.0.0.1'
+    clientport = 20000 + random.randint(1,8000)
+
+    # RFC directory create ADD request for new peer
+    currdir = os.listdir(os.getcwd()+'/RFC')
+    for rfc in currdir:
+        if rfc.startswith('.'):
+            continue
+        file = rfc.split('-')
+        peerRfc[file[0]] = str(file[1])
+    print(peerRfc)
+    details = ""
+    for key,value in peerRfc.items():
+        details += "ADD " + str(key) + " P2P-CI/1.0\n"\
+                "Host: " + str(clienthost)+ "\n"\
+                "Port: " + str(clientport)+ "\n"\
+                "Title: " + str(value) + "\n"
+    sendToServer = [clienthost,clientport,peerRfc,details]
+    sendData = pickle.dumps(sendToServer,-1)
+    s.send(sendData)
+    s.close
+    # Threads for managing download requests from peers which remains active until connection is open
+    start_new_thread(upload,(clienthost,clientport))
+    getUserInput(s,clienthost,clientport)
+
+if __name__ == "__main__":
+    main()
--- server.py
+++ server.py
@@ -2,43 +2,125 @@ import socket
 from _thread import *
 import pickle
 
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.bind((socket.gethostname(),7734))
-s.listen(5)
+def delAllMapping(hname):
+    global rfcMapping, activePeer
+    if activePeer[hname]:
+        del activePeer[hname]
+    for key,value in rfcMapping.items():
+        hosts, title = value
+        if hname in hosts:
+            if len(hosts) == 1:
+                del rfcMapping[key]
+            else:
+                hosts.remove(hname)
+                rfcMapping[key] = (hosts,title)
 
-print("Let's get this server rolling")
+def getResponseAdd(number,title,hname,port):
+    return "P2P-CI/1.0 200 OK\n"+str(number)+" "+str(title)+" "+str(hname)+" "+str(port)
 
-rfcMapping = {} # Key: rfc number and Value is a tuple (list of peers using the rfc, title of rfc)
-activePeer = {} # Key: host name and Value: port used for connection to server
+def getResponseLookup(number,title):
+    global rfcMapping, activePeer
+    response = ""
+    if rfcMapping[number]:
+        hosts, rfcTitle = rfcMapping[number]
+        if len(hosts) > 1:
+            response += "P2P-CI/1.0 200 OK"
+            for h in hosts:
+                getHostInfo = str(number) + " " + title + " " + str(h) + " " + str(activePeer.get(h))
+                response += "\n" + getHostInfo
+        else:
+            response = "No peer has this RFC"
+    else:
+        response = "P2P-CI/1.0 404 Not Found"
+    return response
+
+def createMapping(number,title,hname):
+    global rfcMapping, activePeer
+    if number in rfcMapping:
+        peerList, rfcTitle = rfcMapping[number]
+        peerList.append(hname)
+        rfcMapping[number] = (peerList,rfcTitle)
+    else:
+        peerList = [hname]
+        rfcMapping[number] = (peerList,title)
 
 def manageClientRequest(clientsocket,address):
     global activePeer, rfcMapping
     info = clientsocket.recv(1024)
     dataFromClient = pickle.loads(info)
-    print(dataFromClient)
-
-    # Need to write code for the mappings 
-
-
+    hname = dataFromClient[0]
+    port = dataFromClient[1]
+    activePeer[hname] = port
+    print("The info list of peer for the client")    
+    # Code for the mappings 
+    for key,value in dataFromClient[2].items():
+        if key not in rfcMapping:
+            peer = [hname]
+            rfcMapping[key] = (peer,value)
+        else:
+            peer,title = rfcMapping.get(key)
+            peer.append(hname)
+            rfcMapping[key] = (peer,title)
+    print(rfcMapping)
     while True:
-        if dataFromClient[0] == 'A':
+        response2 = clientsocket.recv(1024).decode('utf-8')
+        print(response2)
+        # case ADD
+        if response2[0] == 'A':
+            print("Inside ADD")
             # list of incoming file
-            res = dataFromClient.split("\n")
+            res = response2.split("\n")
+            print(res)
             # 'ADD RFC 3 P2P-CI/1.0', 'HOST: 127.0.0.1', 'Port: 24520', 'Title: rfc0005'
-            textNeeded = res[0][8] + " " + res[3][7:] + " " + res[1][6:] + " " + res[2][6:]
-            print(textNeeded)
-            #3 RFC003 127.0.0.2 60234
+            rfcNo = res[0][8]
+            rfcTitle = res[3][7:]
+            rfcHost = res[1][6:]
+            #rfcPort = res[2][6:]
+            createMapping(rfcNo,rfcTitle,rfcHost)
+            print("ahsoakdnksandlkasn")
+            print(rfcMapping)
+            # P2P-CI/1.0 200 OK\n1 RFC001 127.0.0.2 61405
+            sendResponse = getResponseAdd(res[0][8],res[3][7:],res[1][6:],res[2][6:])
+            print(sendResponse)
+            clientsocket.send(bytes("P2P-CI/1.0 200 OK \n" + rfcNo + "\n" + rfcTitle + "\n" + rfcHost + "\n",'utf-8'))
+
+        # case LIST
+        elif response2[0] == 'L' and response2[1] == 'I':
             break
 
-        elif dataFromClient[0] == 'E':
+        elif response2[0] == 'L' and response2[1] == 'O':
+            res = response2.split("\n")
+            rfcNo = (res[0].split())[1]
+            rfcTitle = (res[3][7:])
+            msg = getResponseLookup(rfcNo,rfcTitle)
+            print(msg)
+            clientsocket.send(bytes(msg,'utf-8'))
+        # case EXIT
+        elif response2[0] == 'E':
             break
 
     print("Closing connection with the client")
-    clientsocket.close()
+    # Remove all mappings for RFC and peer information
+    delAllMapping(hname)
+    clientsocket.close
 
-while True:
-    clientsocket, address = s.accept()
-    print(f"A new connection has been established from {address}")
-    #clientsocket.send(bytes("Let me in the server", "utf-8"))
-    start_new_thread(manageClientRequest,(clientsocket,address))    
-clientsocket.close()
+def main():
+
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.bind((socket.gethostname(),7734))
+    s.listen(5)
+
+    print("Let's get this server rolling")
+
+    while True:
+        clientsocket, address = s.accept()
+        print(f"A new connection has been established from {address}")
+        #clientsocket.send(bytes("Let me in the server", "utf-8"))
+        start_new_thread(manageClientRequest,(clientsocket,address))    
+    clientsocket.close
+
+rfcMapping = {} # Key: rfc number and Value is a tuple (list of peers using the rfc, title of rfc)
+activePeer = {} # Key: host name and Value: port used for connection to server
+
+if __name__ == "__main__":
+    main()
